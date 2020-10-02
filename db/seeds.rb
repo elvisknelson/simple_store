80e3c22e4bf5901d9b2163b9eb11c8506066d934
Elvis Knelson <elfishpro@hotmail.com>
2020-10-02 16:49:14 -0500

    add faker and populate product table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.destroy_all
+
+PRODUCTS_TO_CREATE = 676
+
+PRODUCTS_TO_CREATE.times do
+  title = Faker::Commerce.product_name
+  price = Faker::Commerce.price
+  stock_quantity = Faker::Number.between(from: 1, to: 3)
+  Product.create(title: title, price: price, stock_quantity: stock_quantity)
+end
+
+puts "Created #{Product.count} Products."
